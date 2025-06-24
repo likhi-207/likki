f332ac512c848de4d4a75aa465d83719652cd9c2
likhi-207 <likhitharudraraju207@gmail.com>
2025-06-24 19:06:45 +0530

    patterns

--- patterns/p8.py
+++ patterns/p8.py
@@ -4,7 +4,7 @@ for i in range(n):
         for i in range(i+1):
             print("*",end=" ")
     if i>n//2:
-        for i in range(n-i+1):
+        for i in range(n-i):
             print("*",end=" ")  
     print()               
 
